741778c18a91e591c1679600a9c75450f738066c
Will Kessler <willkessler@gmail.com>
2018-09-16 19:13:36 -0700

    first working version of transfer_graffitis

--- jupytergraffiti/js/storage.js
+++ jupytergraffiti/js/storage.js
@@ -253,7 +253,7 @@ define([
       let originalGraffitiId;
       if (notebook.metadata.hasOwnProperty('graffitiId')) {
         originalGraffitiId = notebook.metadata.graffitiId;
-        delete(notebook.metadata[graffitiId]);
+        delete(notebook.metadata['graffitiId']);
       }
       storage.ensureNotebookGetsGraffitiId();
       utils.saveNotebook();
@@ -261,7 +261,7 @@ define([
       const notebookPath = "jupytergraffiti_data/notebooks/";
       const sourceTree = notebookPath + originalGraffitiId;
       const destTree = notebookPath + newGraffitiId;
-      const copyPython = "import shutil\nshutil.copytree(" + sourceTree + "," + destTree + ")\n";
+      const copyPython = "import shutil\nshutil.copytree('" + sourceTree + "','" + destTree + "')\n";
       console.log('Graffiti: transferGraffitis will run:', copyPython);
 
       this.Jupyter.notebook.kernel.execute(copyPython,
@@ -271,20 +271,10 @@ define([
                                              store_history: false,
                                              stop_on_error : true
                                            });
-      dialog.modal({
-        title: 'Transfer Complete',
-        body: 'Your Notebook\'s Graffitis have now been copied over from the original notebook. ' +
-              'You can now modify them, or add/remove Graffitis without affecting the original notebook\'s Graffitis',
-        sanitize:false,
-        buttons: {
-          'OK': {
-            click: (e) => {
-              console.log('Graffiti: You clicked ok');
-            }
-          }
-        }
-      });
+
+      return Promise.resolve(); // not really doing this right but...
     },
+
   }
 
   return(storage);
