9828058fa0b8b202e7ab1efce271a29f1c66c53d
Will Kessler <willkessler@gmail.com>
2018-10-08 14:32:26 -0700

    trying to use nested svg's and it's not working

--- jupytergraffiti/js/svg.js
+++ jupytergraffiti/js/svg.js
@@ -30,10 +30,8 @@ define([], () => {
       return el;
     },
 
-    renderSvg: (svgChild, x, y, width, height, viewBox, transform) => {
+    renderSvg: (svgChildren, x, y, width, height, viewBox) => {
       const container = svg.makeSvgElement('svg', {
-        'xmlns': "http://www.w3.org/2000/svg",
-        'version': "1.1",
         'viewbox': viewBox,
         'class':"graffitiSvg",
         'width': width,
@@ -41,18 +39,27 @@ define([], () => {
         'style' : 'left:' + x + 'px;top:' + y + 'px;',
         'preserveAspectRatio':"none"
       });
-      if (transform !== undefined) {
-        container.setAttribute('transform', transform);
+      
+      let innerSvg;
+      for (let svgChild of svgChildren) {
+        innerSvg = svg.makeSvgElement('svg', {
+          'viewbox': svgChild.viewBox,
+          'class':"graffitiSvg",
+          'width': svgChild.width,
+          'height': svgChild.height,
+          'style' : 'left:' + svgChild.x + 'px;top:' + svgChild.y + 'px;',
+          'preserveAspectRatio':"none"
+        });
+        innerSvg.appendChild(svgChild.el);
+        container.appendChild(innerSvg);
       }
-
-      container.appendChild(svgChild);
       let svgGenerator = $('#graffitiSvgGenerator');
       if (svgGenerator.length === 0) {
         $('body').append($('<div id="graffitiSvgGenerator"></div>'));
         svgGenerator = $('#graffitiSvgGenerator');
       }
       svgGenerator[0].appendChild(container);
-      containerHtml = svgGenerator[0].innerHTML;
+      const containerHtml = svgGenerator[0].innerHTML;
       svgGenerator.empty();
 
       return containerHtml;
@@ -96,33 +103,8 @@ define([], () => {
     // need to use html injection, not code generation to make stuff work, 
     // cf my post: https://stackoverflow.com/questions/52675823/preserveaspectratio-ignored-by-code-generation-but-not-html-injection-for-svg-p
 
-    makeRightCurlyBracket: (x, y, height) => {
-      const rightCurlyBracket = 
-        svg.makeElementHtml('path', {
-          "fill": "none",
-          "stroke": "#000",
-          "vector-effect": "non-scaling-stroke",
-          "stroke-width" : "2",
-          "d": "M0,0 A100, 173.20508075688772 0 0 1  100, 173.20508075688772 A100, " + 
-               "173.20508075688772 0 0 0 200 346.41016151377545 A100, " + 
-               "173.20508075688772 0 0 0 100, 519.6152422706632 A100, " +
-               "173.20508075688772 0 0 1 0, 692.8203230275509"
-        });
-
-
-      const container = svg.makeElementHtml('svg', {
-        'width':"8",
-        'height': height,
-        'viewbox':"0 0 200 692",
-        'style' : "left:50px;top:25px;",
-        'preserveAspectRatio':"none"
-      }, rightCurlyBracket);
-
-      return container;
-    },
-
-    makeRightCurlyBracketNew: (x, y, height) => {
-      const rightCurlyBracket =
+    makeLeftCurlyBrace: (x, y, height) => {
+      const leftCurlyBrace =
         svg.makeSvgElement('path',
                            {
                              fill: "none",
@@ -135,10 +117,82 @@ define([], () => {
                                 "173.20508075688772 0 0 1 0, 692.8203230275509"
                            }
         );
-      return svg.renderSvg(rightCurlyBracket,x,y,8,height,"0 0 200 692", "scale(-1,1) translate(-100,10)");
+      return svg.renderSvg(leftCurlyBrace,x,y,8,height,"0 0 200 692", "scale(-1,1) translate(-100,10)");
     },
 
-};
+    makeRightCurlyBrace: (x, y, height) => {
+      const rightCurlyBrace =
+        svg.makeSvgElement('path',
+                           {
+                             fill: "none",
+                             stroke: "#000",
+                             "vector-effect": "non-scaling-stroke",
+                             "stroke-width" : "2",
+                             d: "M0,0 A100, 173.20508075688772 0 0 1  100, 173.20508075688772 A100, " +
+                                "173.20508075688772 0 0 0 200 346.41016151377545 A100, " +
+                                "173.20508075688772 0 0 0 100, 519.6152422706632 A100, " +
+                                "173.20508075688772 0 0 1 0, 692.8203230275509"
+                           }
+        );
+      return svg.renderSvg([rightCurlyBrace], x, y, 8, height, "0 0 200 692");
+
+    },
+
+    makeSymmetricCurlyBraces: (x, y, width, height) => {
+      const halfWidth = width / 2;
+      const leftCurlyBrace =
+        svg.makeSvgElement('path',
+                           {
+                             fill: "none",
+                             stroke: "#000",
+                             "vector-effect": "non-scaling-stroke",
+                             "stroke-width" : "2",
+                             d: "M0,0 A100, 173.20508075688772 0 0 1  100, 173.20508075688772 A100, " +
+                                "173.20508075688772 0 0 0 200 346.41016151377545 A100, " +
+                                "173.20508075688772 0 0 0 100, 519.6152422706632 A100, " +
+                                "173.20508075688772 0 0 1 0, 692.8203230275509",
+//                             transform:"scale(-1,1)" // flip horizontally
+                           }
+        );
+      const rightCurlyBrace =
+        svg.makeSvgElement('path',
+                           {
+                             fill: "none",
+                             stroke: "#000",
+                             "vector-effect": "non-scaling-stroke",
+                             "stroke-width" : "2",
+                             d: "M0,0 A100, 173.20508075688772 0 0 1  100, 173.20508075688772 A100, " +
+                                "173.20508075688772 0 0 0 200 346.41016151377545 A100, " +
+                                "173.20508075688772 0 0 0 100, 519.6152422706632 A100, " +
+                                "173.20508075688772 0 0 1 0, 692.8203230275509"
+                           }
+        );
+      const curlyViewBox = '0 0 200 692';
+      const renderedSvg = svg.renderSvg([
+        { 
+          el: leftCurlyBrace, 
+          viewBox: curlyViewBox,
+          width: 8,
+          height: height,
+          x: 0,
+          y : 0
+        },
+        { 
+          el: rightCurlyBrace, 
+          viewBox: curlyViewBox,
+          width: 8,
+          height: height,
+          x: width - 10,
+          y : 0
+        }
+      ], x,y,width,height,'0 0 ' + width + ' ' + height);
+      console.log(renderedSvg);
+      return renderedSvg;
+    },
+
+    
+    
+  };
   
   return (svg);
 });
