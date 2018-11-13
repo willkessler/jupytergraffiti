36a056c12d66b19635977dcd8e194e6ad85eaab0
tugceakin <tugce.akin@udacity.com>
2018-11-12 17:06:31 -0800

    Change getUser return value to a promise

--- jupytergraffiti/js/udacityUser.js
+++ jupytergraffiti/js/udacityUser.js
@@ -1,11 +1,22 @@
 define([
-  './utils.js'
-], function(utils) {
+  './utils.js',
+  './state.js'
+], function(utils, state) {
         
   // Stub function, in Udacity usage we use a Udacity id instead.
   const udacityUser = {
     getUser: () => {
-      return utils.generateUniqueId();
+      return new Promise((resolve) => { 
+        resolve({
+          userId: utils.generateUniqueId()
+        })
+      })
+    },
+    setUser: () => {
+      udacityUser.getUser()
+      .then(user => {
+        state.setUserId(user.userId)
+      })
     }
   }
 
