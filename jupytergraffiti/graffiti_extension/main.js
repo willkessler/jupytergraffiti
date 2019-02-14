93f84acec0dc1ea9af3372f00c13393cc65b539c
tugceakin <tugce.akin@udacity.com>
2019-02-14 11:27:08 -0800

    Fix extension initialization bugs
    -Wait till notebook is fully loaded to initialize graffiti extension. Also move global if else inside loader module. This was causing issues in bundled js version, causing loader.js always to run along with main.js.

--- jupytergraffiti/graffiti_extension/main.js
+++ jupytergraffiti/graffiti_extension/main.js
@@ -12,17 +12,34 @@ define([
 ], (Jupyter, Graffiti, utils, storage, udacityUser) => {
   function load_ipython_extension() {
     console.log('Graffiti loaded:', Graffiti);
-    window.Graffiti = Graffiti;
-    udacityUser.setUser();
-    Graffiti.init();
 
-    Jupyter.notebook.events.on('kernel_ready.Kernel', (e) => { 
-      console.log('Graffiti: kernel ready, possible kernel restart.', e);
-      console.log('Reloading loader.js');
-      require(['jupytergraffiti/js/loader.js']);
-      utils.saveNotebook();
-    });
-    
+    const initExtension = () => {
+      window.Graffiti = Graffiti;
+      udacityUser.setUser();
+      Graffiti.init();
+  
+      Jupyter.notebook.events.on('kernel_ready.Kernel', () => { 
+        console.log('Graffiti: kernel ready, possible kernel restart.', e);
+        console.log('Reloading loader.js');
+        if (!udacityUser.token) {
+          udacityUser.setUser();
+        } 
+        require(['jupytergraffiti/js/loader.js']);
+        utils.saveNotebook();
+      });
+    }
+
+    // the notebook may have fully loaded before the nbextension gets loaded
+    // so the nbextension would miss the `notebook_loaded.Notebook` event
+    if (Jupyter.notebook._fully_loaded) {
+      console.log('Notebook is already fully loaded.');
+      initExtension();
+    } else {
+      Jupyter.notebook.events.on('notebook_loaded.Notebook', () => {
+        console.log('Notebook is loaded.');
+        initExtension();
+      })
+    }   
   }
 
   return {
