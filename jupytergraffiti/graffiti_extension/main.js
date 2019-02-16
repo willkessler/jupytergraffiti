2e298648d1fa5b770880d798519c0e2a071351ac
Will Kessler <willkessler@gmail.com>
2019-02-15 22:21:30 -0800

    upgraded main.js to check for notebook ready state

--- jupytergraffiti/graffiti_extension/main.js
+++ jupytergraffiti/graffiti_extension/main.js
@@ -10,22 +10,39 @@ define([
   '/nbextensions/graffiti_extension/js/storage.js',
   '/nbextensions/graffiti_extension/js/udacityUser.js'
 ], (Jupyter, Graffiti, utils, storage, udacityUser) => {
+  console.log('Graffiti loaded:', Graffiti);
   function load_ipython_extension() {
-    console.log('Graffiti loaded:', Graffiti);
-    window.Graffiti = Graffiti;
-    udacityUser.setUser();
-    Graffiti.init();
+    const initExtension = () => {
+      window.Graffiti = Graffiti;
+      udacityUser.setUser();
+      Graffiti.init();
+      
+      Jupyter.notebook.events.on('kernel_ready.Kernel', () => { 
+        console.log('Graffiti: kernel ready, possible kernel restart.', e);
+        console.log('Reloading loader.js');
+        if (!udacityUser.token) {
+          udacityUser.setUser();
+        } 
+        require(['jupytergraffiti/js/loader.js']);
+        utils.saveNotebook();
+      });
+    }
 
-    Jupyter.notebook.events.on('kernel_ready.Kernel', (e) => { 
-      console.log('Graffiti: kernel ready, possible kernel restart.', e);
-      console.log('Reloading loader.js');
-      require(['jupytergraffiti/graffiti_extension/main.js']);
-      utils.saveNotebook();
-    });
-    
+    // the notebook may have fully loaded before the nbextension gets loaded
+    // so the nbextension would miss the `notebook_loaded.Notebook` event
+    if (Jupyter.notebook._fully_loaded) {
+      console.log('Notebook is already fully loaded.');
+      initExtension();
+    } else {
+      Jupyter.notebook.events.on('notebook_loaded.Notebook', () => {
+        console.log('Notebook is loaded.');
+        initExtension();
+      })
+    }   
   }
 
   return {
     load_ipython_extension: load_ipython_extension
   };
+
 });
